31dfdb06413a5026b63313664109128a5f8c8765
shafnam <shafi.2288@gmail.com>
2019-07-21 22:00:35 +0930

    Unit test NumberofDaysTest created

---
+++ tests/Unit/NumberOfDaysTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class NumberOfDaysTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     * It tests whether the number of days 
+     * between 9th and 19th is equal to 10 days
+     * @return void
+     * @test 
+     */
+    public function the_number_of_days_between_two_datetime_parameters()
+    {
+        $start_date = '07/09/2019 9:35 PM';
+        $end_date = '07/19/2019 9:35 PM';
+        $start = strtotime($start_date);
+        $end = strtotime($end_date);
+        
+        $expected = 10;        
+        $actual = calculateNumberOfDays($start, $end);
+
+        $this->assertEquals($expected, $actual);
+    }
+}
