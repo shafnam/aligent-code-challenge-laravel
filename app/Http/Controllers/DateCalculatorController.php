58478cfa96b4c4edb297535c24202895a1610749
shafnam <shafi.2288@gmail.com>
2019-07-21 21:03:37 +0930

    DateCalculatorController created

---
+++ app/Http/Controllers/DateCalculatorController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DateCalculatorController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+}
