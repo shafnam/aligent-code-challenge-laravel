7d85962bfd1ffa5277d0243fe2284d5b36e3b020
shafnam <shafi.2288@gmail.com>
2019-07-22 11:46:27 +0930

    Added new unit test case for timezone option

--- tests/Unit/NumberOfDaysTest.php
+++ tests/Unit/NumberOfDaysTest.php
@@ -49,4 +49,28 @@ class NumberOfDaysTest extends TestCase
 
         $this->assertEquals($expected, $actual);
     }
+
+    /**
+     * A basic unit test example.
+     * It tests whether the number of hours 
+     * between 9th and 10th is equal to 1 day or 27 hours 
+     * when start date timezone is UTC/GMT +03:00  -  Africa/Addis_ababa
+     * and end date timezone is UTC/GMT +00:00  -  Africa/Abidjan
+     * @return void
+     * @test 
+     */
+    public function the_number_of_days_between_two_datetime_parameters_with_timezone_convert_option()
+    {
+        date_default_timezone_set('Africa/Addis_ababa'); 
+        $start_date = '07/09/2019 9:35 PM';
+        $end_date = '07/10/2019 9:35 PM';
+        $convert_to = 'hours';
+        $start = strtotime($start_date);
+        $end = strtotime($end_date. ' Africa/Abidjan');
+        
+        $expected = array(1,27);        
+        $actual = calculateNumberOfDays($start, $end, $convert_to);
+
+        $this->assertEquals($expected, $actual);
+    }
 }
